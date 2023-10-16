f376add55b61a6bd735d4be0d4c178525dc57cf8
radimir007 <radovan.psotny123@gmail.com>
2023-10-16 12:22:12 +0200

    commited b.cpp

---
+++ zenit_2022-23/celostatne/b.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+
+int main() {
+    int ding, dong;
+    string input;
+
+    unordered_map <char, int> letters;
+
+    cin >> ding >> dong;
+    cin >> input;
+
+    for (int i = 0; i < input.size()-1; i++) {
+        if (letters.find(input[i]) != letters.end()) {
+            letters.find(input[i])->second++;
+        } else {
+            letters.insert({ input[i], 1 });
+        }
+    }
+
+    for (auto i = letters.begin(); i != letters.end(); i++) {
+        cout << i->first << " : " << i->second << endl;
+    }
+
+    return 0;
+}

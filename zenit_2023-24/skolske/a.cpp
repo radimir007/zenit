ea85d0906ce9d4ff4c272da14a354e300283b58d
radimir007 <radovan.psotny123@gmail.com>
2023-10-17 09:14:27 +0200

    Done A and B

--- zenit_2023-24/skolske/a.cpp
+++ zenit_2023-24/skolske/a.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 
+    cout << "programovania" << endl;
 
     return 0;
 }
--- zenit_2023-24/skolske/b.cpp
+++ zenit_2023-24/skolske/b.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
 
+    string input;
+    getline(cin, input);
+
+    string temp = {};
+
+    int count = 0;
+
+    for (int i = 0; i < input.length()-1; i++) {
+        if (input[i] == '-') {
+            break;
+        }
+
+        temp += input[i];
+        if (temp == "ne") {
+            count++;
+            temp = {};
+        }
+    }
+
+    if (count % 2 == 0) {
+        cout << "ano" << endl;
+    } else {
+        cout << "nie" << endl;
+    }
 
     return 0;
 }
